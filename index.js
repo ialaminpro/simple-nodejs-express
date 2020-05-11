e7288c7ea2dd77b96e34eeb0d7028c84264f637a
Al Amin <ialamin.pro@gmail.com>
2020-05-12 03:18:31 +0600

    dynamic api, api parameter, access params, access query

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+const users = ['Asad', 'Moin','Al Amin'];
 // function rootCall(req, res){
 //     res.send("Thank you very much");
 // }
@@ -13,6 +14,13 @@ app.get('/',(req, res) => {
 
 app.get('/fruits/banana',(req, res) => {
     res.send({fruit: 'banana',quantity: 1000, price: 344})
+})
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const name = users[userId];
+    console.log(req.query.sort);
+    res.send({userId, name});
 })
 
 app.listen(3001, () => console.log('Listening to port 3001'));
