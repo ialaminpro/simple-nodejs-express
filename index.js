f3141941e78abbe4c8c204b7501e7876910ab98f
Al Amin <ialamin.pro@gmail.com>
2020-05-12 03:07:41 +0600

    install nodemon, api return object, create multiple api

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express();
 // const rootCall = (req, res) => res.send("Thank you very much");
 // app.get('/',rootCall);
 app.get('/',(req, res) => {
-    res.send("Thank you very much")
+    res.send("Thank you very much!")
 });
+
+app.get('/fruits/banana',(req, res) => {
+    res.send({fruit: 'banana',quantity: 1000, price: 344})
+})
 
 app.listen(3001, () => console.log('Listening to port 3001'));
